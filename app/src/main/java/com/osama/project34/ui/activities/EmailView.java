582ea92f136fe00cc1dcc49c3ef1e3671d6a4833
patriot <patriot321power@gmail.com>
2017-07-01 23:06:44 +0500

    Manu has been added and design of activity is still in process

---
+++ app/src/main/java/com/osama/project34/ui/activities/EmailView.java
@@ -0,0 +1,24 @@
+package com.osama.project34.ui.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.*;
+
+import com.osama.project34.R;
+
+/**
+ * Created by patriot on 7/1/17.
+ */
+
+public class EmailView extends Activity{
+    @Override
+    public boolean onCreate(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState,Menu menu) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.email_view);
+     //   MenuInflater inflater1 = getMenuInflater();
+       // inflater1.inflate(R.menu.main, menu);
+       // return super.onCreateOptionsMenu(menu);
+
+    }
+}
