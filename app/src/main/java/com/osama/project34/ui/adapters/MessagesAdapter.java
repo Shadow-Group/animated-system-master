4fe3c21b5c81501bb1e3c53f3fa583b3e40dd732
bullhead <m.osamabinomar@gmail.com>
2017-08-18 03:13:16 +0500

    set drawable

--- app/src/main/java/com/osama/project34/ui/adapters/MessagesAdapter.java
+++ app/src/main/java/com/osama/project34/ui/adapters/MessagesAdapter.java
@@ -1,12 +1,16 @@
 package com.osama.project34.ui.adapters;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.amulyakhare.textdrawable.TextDrawable;
+import com.amulyakhare.textdrawable.util.ColorGenerator;
 import com.osama.project34.imap.MessagesDataModel;
 import com.osama.project34.R;
 
@@ -20,9 +24,27 @@ import java.util.ArrayList;
 public class MessagesAdapter extends RecyclerView.Adapter<MessagesAdapter.ViewHolder>{
     private Context mContext;
     private ArrayList<MessagesDataModel> mDataSet;
+    private ArrayList<String> mColors;
+
 
     public MessagesAdapter(Context context){
         this.mContext=context;
+        mColors = new ArrayList<>();
+        mColors.add("#F44336");
+        mColors.add("#E91E63");
+        mColors.add("#9C27B0");
+        mColors.add("#673AB7");
+        mColors.add("#3F51B5");
+        mColors.add("#2196F3");
+        mColors.add("#00BCD4");
+        mColors.add("#009688");
+        mColors.add("#4CAF50");
+        mColors.add("#CDDC39");
+        mColors.add("#FFC107");
+        mColors.add("#FF9800");
+        mColors.add("#FF5722");
+        mColors.add("#795548");
+        mColors.add("#607D8B");
     }
     public void setDataSet(ArrayList<MessagesDataModel> dataset){
         this.mDataSet=dataset;
@@ -36,9 +58,14 @@ public class MessagesAdapter extends RecyclerView.Adapter<MessagesAdapter.ViewHo
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
        MessagesDataModel dataModel=mDataSet.get(position);
+        String name=dataModel.getMessageSender()==null?"Unknown":dataModel.getMessageSender();
         holder.mMessageTextTextView.setText(dataModel.getMessageText());
-        holder.mMessageSenderTextView.setText(dataModel.getMessageSender());
+        holder.mMessageSenderTextView.setText(name);
         holder.mMessageSubjectTextView.setText(dataModel.getMessageSubject());
+        ColorGenerator generator = ColorGenerator.MATERIAL;
+        TextDrawable thumb=TextDrawable.builder()
+                .buildRound(name.substring(0,1), generator.getColor(dataModel.getMessageNumber()));
+        holder.senderThumb.setImageDrawable(thumb);
     }
 
     @Override
@@ -50,6 +77,7 @@ public class MessagesAdapter extends RecyclerView.Adapter<MessagesAdapter.ViewHo
         TextView mMessageSenderTextView;
         TextView mMessageTextTextView;
         TextView mMessageSubjectTextView;
+        ImageView senderThumb;
 
         public ViewHolder(View itemView) {
             super(itemView);
@@ -58,6 +86,7 @@ public class MessagesAdapter extends RecyclerView.Adapter<MessagesAdapter.ViewHo
             mMessageSenderTextView      = (TextView)itemView.findViewById(R.id.list_title_text);
             mMessageTextTextView        = (TextView)itemView.findViewById(R.id.list_message_text);
             mMessageSubjectTextView     = (TextView)itemView.findViewById(R.id.list_subject_text);
+            senderThumb                 = ((ImageView) itemView.findViewById(R.id.list_sender_thumb));
         }
     }
 }
