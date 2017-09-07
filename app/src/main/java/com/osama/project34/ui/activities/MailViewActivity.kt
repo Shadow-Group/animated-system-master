0cb60635dd44dbd3879d62429c7dc5c077cd1c9e
bullhead <m.osamabinomar@gmail.com>
2017-09-07 19:26:06 +0500

    fixed build files for faster builds

--- app/src/main/java/com/osama/project34/ui/activities/MailViewActivity.kt
+++ app/src/main/java/com/osama/project34/ui/activities/MailViewActivity.kt
@@ -1,9 +1,12 @@
 package com.osama.project34.ui.activities
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.View
+import com.amulyakhare.textdrawable.TextDrawable
+import com.amulyakhare.textdrawable.util.ColorGenerator
 import com.google.gson.Gson
-
 import com.osama.project34.R
 import com.osama.project34.data.Mail
 import com.osama.project34.utils.CommonConstants
@@ -17,8 +20,24 @@ class MailViewActivity : AppCompatActivity() {
 
 
         currentMail=Gson().fromJson(intent.extras.getString(CommonConstants.MAIL_VIEW_INTENT),Mail::class.java)
+        viewerMailSubject.text=currentMail!!.subject
+        senderName.text=currentMail!!.sender
+        val generator = ColorGenerator.MATERIAL
+        val thumb = TextDrawable.builder()
+                .buildRound(currentMail!!.sender.substring(0, 1), generator.getColor(currentMail!!.getId()))
+        viewerSenderIcon.setImageDrawable(thumb)
       for (data in currentMail!!.message.text){
            email_viewer.loadData(data,"text/html; charset=utf-8", "UTF-8")
       }
+        setupToolbar()
+        email_viewer.layoutParams.width=resources.displayMetrics.widthPixels
+
     }
+
+    private fun setupToolbar() {
+        val toolbar = findViewById(R.id.main_toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+    }
+
+
 }
