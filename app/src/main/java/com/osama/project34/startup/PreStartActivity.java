7d00c289bab30ee88afe8fb5b85fd1a5ee3d5d23
Shadow <hamzalatif72@gmail.com>
2017-09-08 10:44:38 +0500

    startup instructions

---
+++ app/src/main/java/com/osama/project34/startup/PreStartActivity.java
@@ -0,0 +1,118 @@
+
+
+package com.osama.project34.startup;
+
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.RelativeLayout;
+
+
+import com.osama.project34.R;
+import com.osama.project34.startup.PagerAdapter;
+import com.osama.project34.ui.activities.SplashActivity;
+
+//import me.relex.circleindicator.CircleIndicator;
+
+public class PreStartActivity extends AppCompatActivity {
+    private RelativeLayout  preStartLayout;
+/*
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pre_start);
+
+        preStartLayout =(RelativeLayout)findViewById(R.id.pre_start);
+        setColors(ContextCompat.getColor(this,R.color.colorAccent));
+
+        //set ViewPager adapter
+        ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pagerAdapter);
+        //set pager indicator
+       CircleIndicator indicator = (CircleIndicator)findViewById(R.id.indicator);
+        viewPager.setAdapter(pagerAdapter);
+        indicator.setViewPager(viewPager);
+        changePage(0);
+
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                changePage(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+        findViewById(R.id.pre_start_skip_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onSkipButtonClick(v);
+            }
+        });
+    }
+
+    @ActionHandler(layoutResource = R.id.pre_start_skip_button)
+    public void onSkipButtonClick(View v){
+        //start the intent for the password activity
+        Intent intent=new Intent(this,SplashActivity.class);
+        //start the activity but not let the user get back to this activity
+        startActivity(intent);
+        finish();
+    }
+
+    public void changePage(int num) {
+        switch (num) {
+            case 0:
+                preStartLayout.setBackgroundColor(ContextCompat.getColor(this,R.color.colorAccent));
+                setColors(ContextCompat.getColor(this,R.color.colorAccent));
+                break;
+            case 1:
+                preStartLayout.setBackgroundColor(ContextCompat.getColor(this,R.color.pagercolor2));
+                setColors(ContextCompat.getColor(this,R.color.pagercolor2));
+                setColors(ContextCompat.getColor(this,R.color.pagercolor2));
+                break;
+            case 2:
+                preStartLayout.setBackgroundColor(ContextCompat.getColor(this,R.color.pagercolor3));
+                setColors(ContextCompat.getColor(this,R.color.pagercolor3));
+                setColors(ContextCompat.getColor(this,R.color.pagercolor3));
+                animateButtonAndShow();
+                break;
+
+        }
+    }
+    private void animateButtonAndShow(){
+        ImageButton button= (ImageButton) findViewById(R.id.pre_start_skip_button);
+        button.setVisibility(View.VISIBLE);
+        //animate
+        Animation animation= AnimationUtils.loadAnimation(this,R.anim.done_button_anim);
+        button.startAnimation(animation);
+    }*/
+    private void setColors(int color){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setNavigationBarColor(color);
+            getWindow().setStatusBarColor(color);
+        }
+    }
+
+}
+
+
---
+++ app/src/main/java/com/osama/project34/startup/ViewPagerFragmentTwo.java
@@ -0,0 +1,26 @@
+
+
+package com.osama.project34.startup;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.osama.project34.R;
+
+/**
+ * Created by Shadow on 1/12/2017.
+ */
+
+public class ViewPagerFragmentTwo extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.pager_fragment_two,container,false);
+    }
+
+}
