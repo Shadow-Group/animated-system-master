5860478b9a2633c279d7fcaac95480db7c07bf8e
bullhead <m.osamabinomar@gmail.com>
2017-09-08 13:57:46 +0500

    load email in async task

--- app/src/main/java/com/osama/project34/ui/activities/MailViewActivity.kt
+++ app/src/main/java/com/osama/project34/ui/activities/MailViewActivity.kt
@@ -1,13 +1,17 @@
 package com.osama.project34.ui.activities
 
+import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import com.amulyakhare.textdrawable.TextDrawable
 import com.amulyakhare.textdrawable.util.ColorGenerator
 import com.google.gson.Gson
+import com.osama.project34.MailApplication
 import com.osama.project34.R
 import com.osama.project34.data.Mail
+import com.osama.project34.imap.MultiPartHandler
+import com.osama.project34.ui.widgets.EmailViewer
 import com.osama.project34.utils.CommonConstants
 import kotlinx.android.synthetic.main.activity_mail_view.*
 
@@ -25,17 +29,45 @@ class MailViewActivity : AppCompatActivity() {
         val thumb = TextDrawable.builder()
                 .buildRound(currentMail!!.sender.substring(0, 1), generator.getColor(currentMail!!.id))
         viewerSenderIcon.setImageDrawable(thumb)
-      for (data in currentMail!!.message.text){
-           email_viewer.loadData(data,"text/html; charset=utf-8", "UTF-8")
-      }
+        email_viewer.setWebViewClient(EmailViewer(mail_loading_bar))
+        LoadMailTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR)
         setupToolbar()
-        email_viewer.layoutParams.width=resources.displayMetrics.widthPixels
 
     }
 
     private fun setupToolbar() {
         val toolbar = findViewById(R.id.main_toolbar) as Toolbar
         setSupportActionBar(toolbar)
+    }
+
+    private inner class LoadMailTask : AsyncTask<Void, String, MultiPartHandler>() {
+        override fun doInBackground(vararg p0: Void?): MultiPartHandler? {
+            return try {
+                //load mail content
+                val message: MultiPartHandler = MailApplication.getDb().getMessageContent(currentMail!!.id)
+                for (data in message.text) {
+                    publishProgress(data)
+                }
+                message
+            } catch (ex: Exception) {
+                ex.printStackTrace()
+                null
+            }
+        }
+
+        override fun onProgressUpdate(vararg data: String) {
+            email_viewer.loadData(data[0], "text/html; charset=utf-8", "UTF-8")
+        }
+
+        override fun onPostExecute(result: MultiPartHandler?) {
+            super.onPostExecute(result)
+            if (result != null) {
+                currentMail!!.message = result
+            } else {
+                //show unable to load mail
+            }
+        }
+
     }
 
 
---
+++ app/src/main/java/com/osama/project34/ui/widgets/EmailViewer.java
@@ -0,0 +1,26 @@
+package com.osama.project34.ui.widgets;
+
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+
+/**
+ * Created by bullhead on 9/8/17.
+ */
+
+public class EmailViewer extends WebViewClient {
+    private ProgressBar loadingBar;
+
+    public EmailViewer(ProgressBar loadingBar) {
+        this.loadingBar = loadingBar;
+        loadingBar.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void onPageFinished(WebView view, String url) {
+        super.onPageFinished(view, url);
+        loadingBar.setVisibility(View.GONE);
+    }
+}
