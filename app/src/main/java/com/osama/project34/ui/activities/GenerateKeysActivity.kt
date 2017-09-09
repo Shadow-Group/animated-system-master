a65eeae47d59f6b9947483862623a6612647a5ff
bullhead <m.osamabinomar@gmail.com>
2017-09-10 02:22:00 +0500

    add use exiting keys activity

---
+++ app/src/main/java/com/osama/project34/ui/activities/GenerateKeysActivity.kt
@@ -0,0 +1,14 @@
+package com.osama.project34.ui.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import com.osama.project34.R
+
+class GenerateKeysActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_generate_keys)
+    }
+}
