73dd98b6ad0098df665fa8c7bb908332b310fd2f
bullhead <m.osamabinomar@gmail.com>
2017-09-11 01:36:25 +0500

    fixed about

--- app/src/main/java/com/osama/project34/ui/activities/about/ShowTeamActivity.java
+++ app/src/main/java/com/osama/project34/ui/activities/about/ShowTeamActivity.java
@@ -1,20 +1,30 @@
 package com.osama.project34.ui.activities.about;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.TextView;
 
 import com.osama.project34.R;
 import com.osama.project34.ui.activities.BaseActivity;
+import com.osama.project34.utils.ConfigManager;
 
 
 public class ShowTeamActivity extends BaseActivity {
-    private String mfirstDevelporName = "Osama Bin Omar";
-    private String msecondDevelporsName = "Hamza Muhammad Latif";
-    private String mthirdDevelporsName = "Muahmmad Ahmad Nasim";
-    private String mworkFirstDevelpor = "Encryption Specialis";
-    private String mworkSecondDevelpor = "Design And Implementation";
-    private String mworkThirdDevelpor = "Database specialist";
+    private String mfirstDevelporName = "Osama Bin Omar (bullhead)";
+    private String msecondDevelporsName = "Hamza Muhammad Latif (shadow)";
+    private String mthirdDevelporsName = "Muahammad Ahmad Nasim (patriot)";
+    private String mworkFirstDevelpor = "Encryption library implementation.";
+    private String mworkSecondDevelpor = "UI designer and application interface architect ";
+    private String mworkThirdDevelpor = "IMAP library design and implementation";
+
+    private String firstGithub="https://github.com/mosamabinomar";
+    private String secondGithub="https://github.com/Shadow-Group";
+    private String thirdGithub="https://github.com/patriot321";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,5 +49,40 @@ public class ShowTeamActivity extends BaseActivity {
         if (getSupportActionBar() != null) {
             getSupportActionBar().setTitle("Developers");
         }
+        findViewById(R.id.first_developer_layout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               showAlertForGitHub(firstGithub);
+            }
+        });
+        findViewById(R.id.second_developer_layout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               showAlertForGitHub(secondGithub);
+            }
+        });
+        findViewById(R.id.third_developer_layout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showAlertForGitHub(thirdGithub);
+            }
+        });
+    }
+    private void openDeveloperGithub(String url){
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
+        startActivity(i);
+    }
+    private void showAlertForGitHub(final String url){
+        new AlertDialog.Builder(this, ConfigManager.isDarkTheme()?R.style.DialogStyleDark:R.style.DialogStyleLight)
+                .setMessage("Would you like to check developer's profile on GitHub")
+                .setTitle("GitHub")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        openDeveloperGithub(url);
+                    }
+                })
+                .show();
     }
 }
