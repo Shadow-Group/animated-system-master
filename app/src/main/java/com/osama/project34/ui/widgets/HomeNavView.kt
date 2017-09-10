5868a1b2ef7c6f27daad8e03641eda68af9c7740
bullhead <m.osamabinomar@gmail.com>
2017-09-10 05:55:52 +0500

    added my own navigation view to fit the device screen better

---
+++ app/src/main/java/com/osama/project34/ui/widgets/HomeNavView.kt
@@ -0,0 +1,28 @@
+package com.osama.floatingplayer.widgets
+
+import android.content.Context
+import android.support.design.widget.NavigationView
+import android.util.AttributeSet
+import android.view.View
+
+/**
+ * Created by bullhead on 7/27/17.
+ *
+ */
+class HomeNavView @JvmOverloads constructor(
+        context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : NavigationView(context, attrs, defStyleAttr) {
+    override fun onMeasure(widthSpec: Int, heightSpec: Int) {
+        super.onMeasure(widthSpec, heightSpec)
+        val width=resources.displayMetrics.widthPixels/1.3
+        setMeasuredDimension(width.toInt(),heightSpec)
+        val count = childCount
+        for (i in 0..count - 1) {
+            val v = getChildAt(i)
+            // this works because you set the dimensions of the ImageView to FILL_PARENT
+            v.measure(View.MeasureSpec.makeMeasureSpec(measuredWidth,
+                    View.MeasureSpec.EXACTLY), View.MeasureSpec.makeMeasureSpec(
+                    measuredHeight, View.MeasureSpec.EXACTLY))
+        }
+    }
+}
