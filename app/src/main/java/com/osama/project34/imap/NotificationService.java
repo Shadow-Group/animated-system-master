539854afb1192e69de6aef1efdce65538c83ba50
bullhead <m.osamabinomar@gmail.com>
2017-09-10 13:54:02 +0500

    display message when clicked on notification

--- app/src/main/java/com/osama/project34/imap/NotificationService.java
+++ app/src/main/java/com/osama/project34/imap/NotificationService.java
@@ -1,21 +1,28 @@
 package com.osama.project34.imap;
 
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
+import com.google.gson.Gson;
 import com.osama.project34.MailApplication;
 import com.osama.project34.R;
 import com.osama.project34.data.Mail;
 import com.osama.project34.data.Sender;
+import com.osama.project34.ui.activities.MailViewActivity;
+import com.osama.project34.utils.CommonConstants;
 import com.osama.project34.utils.ConfigManager;
+import com.sun.mail.imap.protocol.FLAGS;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Random;
 
 import javax.mail.Address;
 import javax.mail.Flags;
@@ -29,11 +36,12 @@ import javax.mail.search.SearchTerm;
 
 /**
  * Created by bullhead on 9/10/17.
+ *
  */
 
 public class NotificationService extends Service {
     private static final String TAG = "bullhead_service";
-
+    private int notificationCount=0;
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -68,7 +76,10 @@ public class NotificationService extends Service {
                                 if (message != null) {
                                     long uid = ((UIDFolder) inbox).getUID(message);
                                     Mail mail = makeMailFromMessage(message, FolderNames.ID_INBOX, uid);
+                                    int number=MailApplication.getDb().insertMail(mail);
                                     showNotification(mail);
+                                    boolean isLoading=inbox.getMessageCount()!=number;
+                                    sendMailBroadcast(FolderNames.ID_INBOX,isLoading);
                                 }
                             }
                         }
@@ -101,14 +112,26 @@ public class NotificationService extends Service {
         return unseenMessages;
 
     }
-
+private void sendMailBroadcast(int folderId,boolean isLoading){
+    Intent intent = new Intent(CommonConstants.GOT_MESSAGE_BROADCAST);
+    intent.putExtra(CommonConstants.MESSAGE_FOLDER_ID, folderId);
+    intent.putExtra(CommonConstants.LOADING_INTENT,isLoading);
+    MailApplication.getInstance().sendBroadcast(intent);
+}
     private void showNotification(Mail mail) {
         NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        Intent showMailIntent=new Intent(this, MailViewActivity.class);
+        showMailIntent.putExtra(CommonConstants.MAIL_VIEW_INTENT,new Gson().toJson(mail));
+        showMailIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_NO_HISTORY|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent intent=
+                PendingIntent.getActivity(this,new Random().nextInt(),showMailIntent,PendingIntent.FLAG_UPDATE_CURRENT);
         NotificationCompat.Builder notification = new NotificationCompat.Builder(this)
                 .setContentText("New mail arrived.")
                 .setContentTitle(mail.getSender().getName() == null ? mail.getSender().getMail() : mail.getSender().getName())
-                .setSmallIcon(R.drawable.logo);
-        manager.notify(0, notification.build());
+                .setSmallIcon(R.drawable.logo)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.drawable.ic_markunread_black_24dp))
+                .setContentIntent(intent);
+        manager.notify(notificationCount++, notification.build());
     }
 
     private Mail makeMailFromMessage(Message message, int folderId, long messageUid) throws MessagingException, IOException {
