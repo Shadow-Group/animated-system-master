48100b8476ac245e8ee0265087f3f57361d70687
bullhead <m.osamabinomar@gmail.com>
2017-09-10 05:51:30 +0500

    added fancy text view

---
+++ app/src/main/java/com/osama/project34/ui/widgets/FancyTextView.java
@@ -0,0 +1,31 @@
+package com.osama.project34.ui.widgets;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.support.v7.widget.AppCompatTextView;
+import android.util.AttributeSet;
+
+/**
+ * Created by bullhead on 9/10/17.
+ *
+ */
+
+public class FancyTextView extends AppCompatTextView{
+    public FancyTextView(Context context) {
+        super(context);
+        setTypeFace();
+    }
+
+    public FancyTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setTypeFace();
+    }
+
+    public FancyTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        setTypeFace();
+    }
+    private void setTypeFace(){
+        setTypeface(Typeface.createFromAsset(getContext().getAssets(),"fonts/Mad.ttf"));
+    }
+}
